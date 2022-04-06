3fb33f881ca4fd2f354afe4391acea5f5c2a1436
CH TALHA <100463587+mtalhach@users.noreply.github.com>
2022-04-06 22:47:09 +0500

    pointers in C++

---
+++ Q8.cpp
@@ -0,0 +1,23 @@
+// Passing by reference without pointers
+
+#include <iostream>
+using namespace std;
+void swap(int &n1, int &n2)
+{
+    int temp = n1;
+    n1 = n2;
+    n2 = temp;
+}
+int main()
+{
+    int a = 5;
+    int b = 20;
+    cout << "Before Swapping:\n";
+    cout << "a is: " << a << endl;
+    cout << "b is: " << b << endl;
+
+    swap(a, b);
+    cout << "\nAfter Swapping:\n";
+    cout << "a is: " << a << endl;
+    cout << "b is: " << b << endl;
+}
